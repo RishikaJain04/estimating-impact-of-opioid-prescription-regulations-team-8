40997040a3209b16bea752efb8cb1d1678d02460
RishikaJain04 <56404780+RishikaJain04@users.noreply.github.com>
2019-11-09 14:05:58 -0500

    Update PreparingMortalityDataToMerge.py

--- 10_code/PreparingMortalityDataToMerge.py
+++ 10_code/PreparingMortalityDataToMerge.py
@@ -2,3 +2,10 @@
 
 os.chdir("C:/Users/Felipe/Desktop/Duke MIDS/Practical Tools in Data Science/")
 cod = pd.read_csv("drug_data_full.csv") #Cause of death
+cod = drug_data_full.drop((['Drug/Alcohol Induced Cause','Notes', 'Year Code','County']), axis=1)
+cod.rename(columns = {'County Code':'FIPS'}, inplace = True)
+dd = cod.dropna(how='all')
+mortality_data = dd[(dd['Drug/Alcohol Induced Cause Code'] != 'A9') &
+(dd['Drug/Alcohol Induced Cause Code'] != 'D9') &
+(dd['Drug/Alcohol Induced Cause Code'] != 'O9')]
+mortality_data
