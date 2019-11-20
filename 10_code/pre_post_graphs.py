c7497750fb52cc6861d7f6f15e6a822caf1fb4fa
felbuch <felbuch@gmail.com>
2019-11-19 23:04:48 -0300

    Modify graphs to do pre_post_graphs and merged data with population 2010

--- 10_code/pre_post_graphs.py
+++ 10_code/pre_post_graphs.py
@@ -2,17 +2,20 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-os.chdir('C:\\Users\\Felipe\\Desktop\\Duke MIDS\\Practical Tools in Data Science\\estimating-impact-of-opioid-prescription-regulations-team-8/20_intermediate_files')
+os.chdir('C:\\Users\\Felipe\\Desktop\\Duke MIDS\\Practical Tools in Data Science\\estimating-impact-of-opioid-prescription-regulations-team-8/20_intermediate_files\\')
 data = pd.read_parquet("merged_data_pop2010.gzip")
 
 
+assert is_primary_key(data,['STATE','YEAR'])
+
+#data.describe()
 
 def pre_post_graph(state, year_of_treatment, df = data, path = 'C:\\Users\\Felipe\\Desktop\\Duke MIDS\\Practical Tools in Data Science\\estimating-impact-of-opioid-prescription-regulations-team-8\\30_results'
 ):
 
-'''This functions makes the pre_post analysis graph for a state which has gone through
-a policy change in the year of treatment.
-This function both plots the graph and saves it as a jpg file in the folder listed in path.'''
+    '''This functions makes the pre_post analysis graph for a state which has gone through
+    a policy change in the year of treatment.
+    This function both plots the graph and saves it as a jpg file in the folder listed in path.'''
 
     treated = df.loc[df.STATE == state,['YEAR','deaths_per_100k']]
     control = df.loc[df.STATE != state,['YEAR','deaths_per_100k']].groupby('YEAR', as_index = False).mean()
@@ -32,7 +35,7 @@ This function both plots the graph and saves it as a jpg file in the folder list
     fig.savefig(state + ".jpg")
     pass
 
-os.getcwd()
+
 #Now lets run our function for the cases we wish to analyse
 pre_post_graph('FL',2010)
 pre_post_graph('WA',2012)
